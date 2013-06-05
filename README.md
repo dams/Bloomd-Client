42e84036ce4a05ccfd4627cdb5e8ece9da934761
Damien Krotkine <dkrotkine@weborama.com>
2013-06-05 13:56:42 +0200

    documentation

--- README.md
+++ README.md
@@ -8,20 +8,18 @@ Installation
 
 Basic installation
 
-```
-perl Makefile.PL
-make
-make install
-```
-
-This distribution uses [DistZilla](http://dzil.org/), so you can use the dzil commands as well.
-
-If you want to run the tests, you'll need a bloomd server up and running, and
-you'll need to set BLOOMD_HOST and BLOOMD_PORT:
+This distribution is on CPAN, so you might want to use your preferred CPAN
+client to install it:
 
 ```
-BLOOMD_HOST=127.0.0.1 BLOOMD_PORT=8673 make test
+# using cpanminus
+cpanm Bloomd::Client
+
+# using regular cpan
+cpan Bloom::Client
 ```
+
+If you'd like to install it from the source, see the last section of this file
 
 Usage
 -----
@@ -32,6 +30,7 @@ with the same name. Return values are converted to Perl types (e.g. `1`/`<empty
 string>` instead of `Yes`/`No`)
 
 ```
+use feature ':5.12';
 use Bloomd::Client;
 my $b = Bloomd::Client->new;
 my $filter = 'test_filter';
@@ -47,9 +46,28 @@ Timeout support
 ---------------
 
 You can set the timeout option to the constructor. The timeout will be on
-reading and on writing the socket.
+reading and on writing to the socket. It can be a float, up to microseconds.
 
 More doc
 --------
 
 Check on metacpan
+
+Build from the source
+---------------------
+
+This distribution uses [DistZilla](http://dzil.org/), so you should first
+install `Dist::Zilla` ( with `cpan Dist::Zilla` or using `cpanm`). Then:
+
+```
+dzil authordeps --missing | cpan
+dzil listdeps --missing | cpan
+dzil build
+```
+
+If you want to run the tests, you'll need a bloomd server up and running, and
+you'll need to set BLOOMD_HOST and BLOOMD_PORT:
+
+```
+BLOOMD_HOST=127.0.0.1 BLOOMD_PORT=8673 dzil test
+```
--- lib/Bloomd/Client.pm
+++ lib/Bloomd/Client.pm
@@ -1,5 +1,7 @@
 package Bloomd::Client;
 
+# ABSTRACT: Perl client to the bloomd server
+
 use feature ':5.12';
 use Moo;
 use Method::Signatures;
@@ -10,11 +12,53 @@ use IO::Socket::INET;
 use POSIX qw(ETIMEDOUT EWOULDBLOCK EAGAIN strerror);
 use Config;
 
+=attr protocol
+
+The protocol. ro, defaults to 'tcp'
+
+=cut
+
 has protocol => ( is => 'ro', default => sub {'tcp'} );
+
+=attr host
+
+The host to connect to. ro, defaults to '127.0.0.1'
+
+=cut
+
 has host => ( is => 'ro', default => sub {'127.0.0.1'} );
+
+=attr port
+
+The port to connect to. ro, defaults to '8673'
+
+=cut
+
 has port => ( is => 'ro', default => sub {8673} );
 has _socket => ( is => 'lazy', predicate => 1, clearer => 1 );
+
+=attr timeout
+
+The timeout (on read and write), in seconds. Can be a float. ro, defaults to 10.
+
+=cut
+
 has timeout => ( is => 'ro', predicate => 1, default => sub { 10 } );
+
+=head1 SYNOPSIS
+
+  use feature ':5.12';
+  use Bloomd::Client;
+  my $b = Bloomd::Client->new( timeout => 0.2 );
+  my $filter = 'test_filter';
+  $b->create($filter);
+  my $array_ref = $b->list();
+  my $hash_ref = $b->info($filter);
+  $b->set($filter, 'u1');
+  if ($b->check($filter, 'u1')) { say "it exists!" }
+  my $hashref = $b->multi( $filter, qw(u1 u2 u3) );
+
+=cut
 
 method _build__socket {
     my $socket = IO::Socket::INET->new(
@@ -44,11 +88,31 @@ method _build__socket {
 
 }
 
+=method disconnect
+
+Closes the connection and reset the internal socket
+
+=cut
+
 method disconnect {
     $self->_has_socket
       and $self->_socket->close;
     $self->_clear_socket;
 }
+
+=method create
+
+Creates a new bloom filter
+
+  $b->create($name, $capacity?, $probability?, $in_memory=0|1?)
+
+Only the name is mandatory. You can specify the initial capacity (if not the
+filter will be enlarged as needed), the probability of maximum false positives
+you want, and a flag to force the filter to not be in memory (by default it is).
+
+Returns true if the filter didn't exist and was created successfully.
+
+=cut
 
 method create ($name, $capacity?, $prob?, $in_memory?) {
     my $args =
@@ -57,6 +121,15 @@ method create ($name, $capacity?, $prob?, $in_memory?) {
       . ( $in_memory ? "in_memory=$in_memory" : '' );
     $self->_execute("create $name $args" ) eq 'Done';
 }
+
+=method list
+
+  my $array_ref = $b->list($prefix?)
+
+Returns an ArrayRef of the existing filters. You can provide a prefix to filter
+on them.
+
+=cut
 
 method list ($prefix? = '') {
     my @keys = qw(name prob size capacity items);
@@ -69,22 +142,67 @@ method list ($prefix? = '') {
     ];
 }
 
+=method drop
+
+  $b->drop($name)
+
+Drops a filter. Returns true if the filter existed and was removed properly.
+
+=cut
+
 method drop ($name) {
     $self->_execute("drop $name") eq 'Done';
 }
 
 
+=method close
+
+  $b->close($name)
+
+Closes a filter. Returns true on success.
+
+=cut
+
 method close ($name) {
     $self->_execute("close $name") eq 'Done';
 }
+
+=method clear
+
+  $b->clear($name)
+
+Clears a filter. Returns tru on success.
+
+=cut
 
 method clear ($name) {
     $self->_execute("clear $name") eq 'Done';
 }
 
+=method check
+
+  if ($b->check($name, $key)) {
+    print "the element $key matched\n";
+  }
+
+Given a filter name and a key name, returns true if the key was previously
+added in the filter (using C<set>).
+
+=cut
+
 method check ($name, $key) {
     $self->_execute("c $name $key") eq 'Yes';
 }
+
+=method multi
+
+  my $hash_ref = $b->multi($name, $key1, key2, key3);
+
+Given a filter name and a list of elements, returns a HashRef, which keys are
+the elements, and the values are 1 or 0, depending if the element is present in
+the filter or not.
+
+=cut
 
 method multi ($name, @keys) {
     @keys
@@ -93,9 +211,26 @@ method multi ($name, @keys) {
     +{mesh @keys, @values };
 }
 
+=method set
+
+  $b->set($name, $key);
+
+Adds the element to the given filter. Returns 1 if the elements was not
+previously in the filter and was properly added.
+
+=cut
+
 method set ($name, $key) {
     $self->_execute("s $name $key") eq 'Yes';
 }
+
+=method bulk
+
+  $b->bulk($name, $key1, $key2, $key3);
+
+Adds the elements to the given filter. Returns void
+
+=cut
 
 method bulk ($name, @keys) {
     @keys
@@ -104,9 +239,25 @@ method bulk ($name, @keys) {
     return;
 }
 
+=method info
+
+  my $hash_ref = $b->info($name);
+
+Returns a HashRef giving informations about the given filter name.
+
+=cut
+
 method info ($name) {
     +{ map { split / / } $self->_execute("info $name") };
 }
+
+=method flush
+
+  $b->flush($name);
+
+Flushes the filter. Returns true on success.
+
+=cut
 
 method flush ($name) {
     $self->_execute("info $name") eq "Done";
@@ -118,17 +269,12 @@ method _execute ($command) {
      $socket->print($command . "\r\n")
        or croak "couldn't write to socket";
 
-#     say STDERR " -- DEBUG0: $!";
      my $line = $self->_check_line($socket->getline);
 
-#     say STDERR " -- DEBUG1: $!";
-#     say STDERR " -- DEBUG1: $line";
-     
      $line = $line->rtrim("\r\n");
      $line =~ /^Client Error:/
        and croak "$line: $command";
 
-#     say "[$line]";
      $line eq 'START'
        or return $line;
 
@@ -136,7 +282,6 @@ method _execute ($command) {
      push @lines, $line
        while ( ($line = $self->_check_line($socket->getline)->rtrim("\r\n")) ne 'END');
  
-#     say Dumper(\@lines); use Data::Dumper;
      return @lines;
 }
 
